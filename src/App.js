605be22f4f5530829429503445d0a502eea16540
ShiyaoC <shiyao1@ualberta.ca>
2022-04-11 15:59:08 -0600

    add account verification

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import SignMessage from "./SignMessage";
+import VerifyMessage from "./VerifyMessage";
+
+export default function App() {
+  return (
+    <div className="flex flex-wrap">
+      <div className="w-full lg:w-1/2">
+        <p>
+          Hello 
+        </p>
+        <SignMessage />
+      </div>
+      <div className="w-full lg:w-1/2">
+        <VerifyMessage />
+      </div>
+    </div>
+  );
+}
