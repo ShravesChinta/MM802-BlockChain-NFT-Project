623ec6c940d9a0ec3c00b1e8bee21340cd427c50
Sravanti Chinta <91924223+ShravesChinta@users.noreply.github.com>
2022-05-31 15:19:47 -0600

    Add files via upload

---
+++ ClientGUI/src/chat/components/AddNewChat.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useState } from "react";
+import { Button, Modal, Form } from "react-bootstrap";
+import LAMOSE_ADDRESS from "../../lamoseInfo"
+
+// This Modal help Add a new friend
+export function AddNewChat(props) {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <div
+      className="AddNewChat"
+      style={{
+        position: "absolute",
+        bottom: "0px",
+        padding: "10px 45px 0 45px",
+        margin: "0 95px 0 0",
+        width: "97%",
+      }}
+    >
+      <Button variant="success" className="mb-2" onClick={handleShow}>
+        + NewChat
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title> Add New Friend </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+       <div>Adding Lamose as your friend</div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => {
+              props.addHandler(
+                "Lamose",
+                {LAMOSE_ADDRESS}
+              );
+              handleClose();
+            }}
+          >
+            Add Friend
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
